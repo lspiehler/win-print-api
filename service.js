1145c38c8d977920ca531236cd8a29b18bdb5451
Lyas Spiehler <lyas.spiehler@sapphirehealth.org>
2021-09-27 10:10:32 -0500

    change cwd

--- service.js
+++ service.js
@@ -4,7 +4,13 @@ var Service = require('node-windows').Service;
 var svc = new Service({
     name:'Print Management Service',
     description: 'Print Management Service',
-    script: 'C:\\win-print-api\\bin\\www'
+    script: 'C:\\win-print-api\\bin\\www',
+    env: [
+        {
+          name: 'APP_DIR_PATH',
+          value: 'C:\\win-print-api'
+        }
+      ]
 });
 
 // Listen for the "install" event, which indicates the
