0fc47469acd493fabf7c043bd998e2d1aa34bae3
Lyas Spiehler <lyas.spiehler@sapphirehealth.org>
2021-09-30 15:00:45 -0500

    config can now accommodate windows service

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').join(__dirname, '.env')});
 const os = require("os");
+//var EventLogger = require('node-windows').EventLogger;
+//const util = require('util');
+
+//var log = new EventLogger('win-print-api');
+
+//log.info(__dirname);
+//log.info('this is a test');
 
 module.exports = {
     MANAGEMENTURL: process.env.MANAGEMENTURL || 'http://192.168.1.200:80',
