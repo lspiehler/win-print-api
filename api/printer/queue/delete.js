38ee61b6e4735da142ca3a1518d509cc6666c229
Lyas Spiehler <lyas.spiehler@sapphirehealth.org>
2021-10-07 06:24:33 -0500

    added queue delete

---
+++ api/printer/queue/delete.js
@@ -0,0 +1,20 @@
+const powershell = require('../../../lib/powershell');
+
+module.exports = function(params, callback) {
+    powershell.runCommand({ cmd: 'Remove-Printer -Name "' + params.name + '"', waitstdout: false }, function(err, portresp) {
+        if(err) {
+            callback(err, false);
+        } else {
+            let resp = {
+                status: 200,
+                headers: [],
+                body: {
+                    result: 'success',
+                    message: 'Printer deleted successfully',
+                    data: null
+                }
+            }
+            callback(false, resp);
+        }
+    });
+}
