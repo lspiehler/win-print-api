980819d7338de2b546ae7251b42130d5c31d42df
Lyas Spiehler <lyas.spiehler@sapphirehealth.org>
2021-10-13 05:40:52 -0500

    added set-printer functionality

--- api/printer/queue/index.js
+++ api/printer/queue/index.js
@@ -3,11 +3,13 @@ const qcreate = require('./create');
 const qdelete = require('./delete');
 const qsetconfig = require('./setconfig');
 const qgetconfig = require('./getconfig');
+const qset = require('./set');
 
 module.exports = {
     list: qlist,
     create: qcreate,
     delete: qdelete,
     setconfig: qsetconfig,
-    getconfig: qgetconfig
+    getconfig: qgetconfig,
+    set: qset
 }
