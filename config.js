2460e00d8f5b215d5f3fc0b607036652a11f5482
Lyas Spiehler <lyas.spiehler@sapphirehealth.org>
2021-11-17 10:42:06 -0600

    add support for wmi

--- config.js
+++ config.js
@@ -8,9 +8,24 @@ const os = require("os");
 //log.info(__dirname);
 //log.info('this is a test');
 
+function getBoolean(str) {
+	if(str) {
+		if(str.toUpperCase()=='TRUE') {
+			return true;
+		} else if(str.toUpperCase()=='FALSE') {
+			return false;
+		} else {
+			return str;
+		}
+	} else {
+		return false;
+	}
+}
+
 module.exports = {
     MANAGEMENTURL: process.env.MANAGEMENTURL || 'http://192.168.1.200:80',
     LISTENPORT: parseInt(process.env.LISTENPORT) || 3000,
     HOSTNAME: process.env.HOSTNAME || os.hostname(),
-    DOMAIN: process.env.DOMAIN || null
+    DOMAIN: process.env.DOMAIN || null,
+    USEWMI: getBoolean(process.env.USEWMI) || false
 }
