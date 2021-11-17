b7a8f8c8118f975760e3834557cf335732804ee1
Lyas Spiehler <lyas.spiehler@sapphirehealth.org>
2021-11-17 08:20:08 -0600

    add PrinterHostAddress port property to printers

--- api/printer/port/list.js
+++ api/printer/port/list.js
@@ -1,61 +1,29 @@
-const powershell = require('../../../lib/powershell');
-
-var cachedresp;
-var reqcallbacks = [];
+const port = require('../../../lib/port');
 
 module.exports = function(params, callback) {
-    if(cachedresp && params.updatecache != true) {
-        let resp = {
-            status: 200,
-            headers: [],
-            body: {
-                result: 'success',
-                message: null,
-                data: cachedresp
-            }
-        }
-
-        callback(false, resp);
-    } else {
-        if(reqcallbacks.length == 0) {
-            cachedresp = false;
-            reqcallbacks.push(callback);
-            powershell.runCommand({ cmd: 'Get-PrinterPort | ConvertTo-Json' }, function(err, portresp) {
-                if(err) {
-                    callback(err, false);
-                } else {
-                    let ports = JSON.parse(portresp.stdout.toString());
-                    cachedresp = {};
-                    for(let i = 0; i <= ports.length - 1; i++) {
-                        /*if(cachedresp.hasOwnProperty(ports[i].name)) {
-
-                        }*/
-                        delete ports[i].CimSystemProperties;
-                        delete ports[i].CimInstanceProperties;
-                        delete ports[i].CimClass;
-                        cachedresp[ports[i].Name] = ports[i];
-                    }
-                    //console.log(cachedresp);
-
-                    let resp = {
-                        status: 200,
-                        headers: [],
-                        body: {
-                            result: 'success',
-                            message: null,
-                            data: cachedresp
-                        }
-                    }
-
-                    while(reqcallbacks.length >= 1) {
-                        let cb = reqcallbacks.shift();
-                        cb(false, resp);
-
-                    }
+    port.list(params, function(err, response) {
+        if(err) {
+            let resp = {
+                status: 500,
+                headers: [],
+                body: {
+                    result: 'error',
+                    message: err,
+                    data: null
                 }
-            });
+            }
+            callback(false, resp);
         } else {
-            reqcallbacks.push(callback);
+            let resp = {
+                status: 200,
+                headers: [],
+                body: {
+                    result: 'success',
+                    message: null,
+                    data: response
+                }
+            }
+            callback(false, resp);
         }
-    }
+    });
 }
